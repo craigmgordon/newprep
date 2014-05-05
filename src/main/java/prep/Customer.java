ad825da01dedbef86088a4e1c713c19bcf8ba7d5
Craig Gordon <craigmgordon@gmail.com>
2014-05-05 20:30:16 +0100

    First commit amended twice

---
+++ src/main/java/prep/Customer.java
@@ -0,0 +1,39 @@
+package prep;
+
+import java.math.BigDecimal;
+
+public class Customer {
+	private String foreame;
+	private String surname;
+	private String customerRef;
+	private BigDecimal salary;
+	
+	public String getForeame() {
+		return foreame;
+	}
+	public void setForeame(String foreame) {
+		this.foreame = foreame;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getCustomerRef() {
+		return customerRef;
+	}
+	public void setCustomerRef(String customerRef) {
+		this.customerRef = customerRef;
+	}
+	public BigDecimal getSalary() {
+		return salary;
+	}
+	public void setSalary(BigDecimal salary) {
+		this.salary = salary;
+	}
+	//further test and again
+	
+	
+	
+}
