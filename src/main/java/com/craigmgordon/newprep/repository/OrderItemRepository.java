e7f72a1597ee03222cf1a71227bda4a11428acf2
Craig Gordon <craigmgordon@gmail.com>
2014-05-21 17:22:31 +0100

    Remove repository implementations for Order and OrderItem and convert repository interfaces to extend JPA Repository

--- src/main/java/com/craigmgordon/newprep/repository/OrderItemRepository.java
+++ src/main/java/com/craigmgordon/newprep/repository/OrderItemRepository.java
@@ -1,9 +1,11 @@
 package com.craigmgordon.newprep.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.craigmgordon.newprep.model.OrderItem;
 
-public interface OrderItemRepository {
-
-	OrderItem save(OrderItem orderItem);
+@Repository("orderItemRepository")
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
--- src/main/java/com/craigmgordon/newprep/repository/OrderRepository.java
+++ src/main/java/com/craigmgordon/newprep/repository/OrderRepository.java
@@ -1,8 +1,11 @@
 package com.craigmgordon.newprep.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.craigmgordon.newprep.model.Order;
 
-public interface OrderRepository {
+@Repository("orderRepository")
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
-	Order save(Order order);
 }
