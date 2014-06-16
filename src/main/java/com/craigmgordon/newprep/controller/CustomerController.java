7db5d1481e55273cdeff45b1db45a9e034b9b935
Craig Gordon <craigmgordon@gmail.com>
2014-06-16 12:28:31 +0100

    Create add new customer form and wire to service

--- src/main/java/com/craigmgordon/newprep/controller/CustomerController.java
+++ src/main/java/com/craigmgordon/newprep/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.craigmgordon.newprep.model.Customer;
@@ -22,6 +23,18 @@ public class CustomerController {
 		model.addAttribute("customers", customers);
 
 		return "customerList";
+	}
+
+	@RequestMapping(value = "/addCustomer")
+	public String addCustomer(@ModelAttribute("customer") Customer customer) {
+
+		// TODO blank record is being saved as well as new record
+		Customer newCustomer = service.save(customer);
+
+		System.out.println("customer forename: " + newCustomer.getForeame());
+		System.out.println("customer surname: " + newCustomer.getSurname());
+		System.out.println("customer ref: " + newCustomer.getCustomerRef());
+		return "addCustomer";
 	}
 
 }
